d88f966ac35e69245d975555b85a2ea440d7f9c0
Masha <vorobeva.asya@mail.ru>
2020-03-19 16:36:36 +0400

    Функция нахождения гипотенузы

--- lesson2/hypotenuse.php
+++ lesson2/hypotenuse.php
@@ -2,13 +2,19 @@
 
 $a=5;
 $b=10;
-$k=($a*$a)+($b*$b);
-$c=sqrt($k);
+$c=Hypotenuse($a,$b);
 echo 'Длина стороны A:'.' '. $a;
 echo '<br>';
 
 echo 'Длина стороны B:'.' '. $b;
-;echo '<br>';
+echo '<br>';
 
 echo 'Длина стороны C:'.' '. $c;
-;echo '<br>';
+echo '<br>';
+echo '<hr>';
+
+function Hypotenuse($l, $k) {
+    $c=sqrt(pow($l,2)+pow($k,2));
+    return $c;
+}
+
